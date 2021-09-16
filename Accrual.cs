27c18a56aaa78d0b2ba7d52a19a01018b248fcbe
Alkidus <68506257+Alkidus@users.noreply.github.com>
2021-09-16 15:04:25 +0300

    Add files via upload

---
+++ Accrual.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Accrual //начисление абонплаты
+    {
+        public Accrual()
+        {
+            Subscribers = new HashSet<Subscriber>();
+        }
+        public int Id { get; set; }
+        public DateTime SumPlusDate { get; set; } //дата начисления абонплаты
+        public float SumPlus { get; set; } //сумма начисленной абонплаты
+        public int SubscriberId { get; set; }
+        public string Comments { get; set; } //коментарии
+        //public virtual Subscriber Subscriber { get; set; }
+        public virtual ICollection<Subscriber> Subscribers { get; set; }
+    }
+}
